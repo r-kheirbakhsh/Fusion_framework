51e5e79c137c3ee5bc9892c97a82e88427afab88
reyhaneh.kheirbakhsh@ucdconnect.ie <reyhaneh@mllabs-gpu-server2.ucd.ie>
2025-09-30 11:36:13 +0000

    update

--- config/arguments.py
+++ config/arguments.py
@@ -3,36 +3,36 @@ import argparse
 
 def parse_args():
     args = argparse.ArgumentParser(description='Modality fusion framework')
-    
-    args.add_argument('--project_name', default=None, type=str)
-    args.add_argument('--fusion_strategy', default='ELF', type=str) # it consists of ['ELF', 'ERF', 'ISF', 'IMF', 'L']
-    args.add_argument('--dataset_csv_path', default=None, type=str)   # the path to the csv file containing the metadata of each slice (columns: ID,slice_name,sex,age,WHO_grade)
-    args.add_argument('--train_csv_path', default=None, type=str)  # the path to the csv file containing the metadata of each slice for training (if )
-    args.add_argument('--val_csv_path', default=None, type=str)
-    args.add_argument('--test_csv_path', default=None, type=str)
-    args.add_argument('--dataset_image_path', default=None, type=str)  # the path to the main folder contaning the sliced MRI scans 
-    args.add_argument('--label', default=4, type=int)    # LABEL2 on metadata after slice for new dataloader (WHO_grade->4 ,final_diagnosis->5 ,MGMT_status->6 ,1p/19q->7 ,IDH->8)
-    args.add_argument('--num_class', default=2, type=int)
+
+    args.add_argument('--project_name', default=None, type=str)         # the name of the project, it will be used for logging to Wandb
+    args.add_argument('--fusion_strategy', default='ELF', type=str)     # it consists of ['ELF', 'ERF', 'ISF', 'IMF', 'L']
+    args.add_argument('--dataset_csv_path', default=None, type=str)     # the path to the csv file containing the metadata of each slice (columns: ID,slice_name,sex,age,WHO_grade)
+    args.add_argument('--train_csv_path', default=None, type=str)       # the path to the csv file containing the metadata of each slice for training (if needed)
+    args.add_argument('--val_csv_path', default=None, type=str)         # the path to the csv file containing the metadata of each slice for validation (if needed)
+    args.add_argument('--test_csv_path', default=None, type=str)        # the path to the csv file containing the metadata of each slice for testing (if needed)
+    args.add_argument('--dataset_image_path', default=None, type=str)   # the path to the main folder contaning the sliced MRI scans 
+    args.add_argument('--label', default=4, type=int)       # LABEL2 on metadata after slice for new dataloader (WHO_grade->4 ,final_diagnosis->5 ,MGMT_status->6 ,1p/19q->7 ,IDH->8)
+    args.add_argument('--num_class', default=2, type=int)   # for binary classification:2
     args.add_argument('--axis', default=2, choices=[0,1,2], type=int)    # axis of the slices -> 0: Sagittal, 1: Coronal, 2: Axial
-    args.add_argument('--modalities', nargs='+', type=str, help="List of modalities for late fuaion") # e.g. ['T1_bias', 'T2_bias', 'Clinical']
-    args.add_argument('--scale_clinical_modality', default='Minmax', type=str) # e.g. Minmxa, Normalize   
-    args.add_argument('--mri_model', default="DenseNet121", type=str) # the architecture of NN for MRI encoder
-    args.add_argument('--cl_model', default="AutoInt", type=str) # the architecture of NN for the Clinical encoder
-    args.add_argument('--fused_model', default="MLP", type=str) # the model for fusion of the modalities it consists of ['MLP', 'AutoInt', 'dneseNet121', 'Inter_1_concat', 'Inter_2_concat', 'Inter_1_concat_attn']
-    args.add_argument('--pretrained', default=0, type=int)   # pretrained mri_model: 1, non-pretrained mri_model: 0 
-    args.add_argument('--lr_mri', default=1e-6, type=float)   # learning rate for mri_model
-    args.add_argument('--lr_cl', default=1e-6, type=float)   # learning rate for cl_model
+    args.add_argument('--modalities', nargs='+', type=str, help="List of modalities for late fusion") # e.g. ['Clinical', 'T1_bias', 'T1c_bias', 'T2_bias', 'FLAIR_bias']
+    args.add_argument('--scale_clinical_modality', default='Minmax', type=str)  # e.g. Minmax
+    args.add_argument('--mri_model', default="DenseNet121", type=str)   # the architecture of NN for MRI encoder
+    args.add_argument('--cl_model', default="AutoInt", type=str)        # the architecture of NN for the Clinical encoder
+    args.add_argument('--fused_model', default="MLP", type=str)         # the model for fusion of the modalities it consists of ['MLP', 'AutoInt', 'dneseNet121', 'Inter_1_concat', 'Inter_2_concat', 'Inter_1_concat_attn']
+    args.add_argument('--pretrained', default=0, type=int)      # pretrained mri_model: 1, non-pretrained mri_model: 0 
+    args.add_argument('--lr_mri', default=1e-6, type=float)     # learning rate for mri_model
+    args.add_argument('--lr_cl', default=1e-6, type=float)      # learning rate for cl_model
     args.add_argument('--lr_fused', default=1e-6, type=float)   # learning rate for fused_model
     args.add_argument('--lmbda', default=1e-4, type=float)
-    args.add_argument('--batch_size_mri', default=16, type=int)   # number of batch size for mri_model
-    args.add_argument('--batch_size_cl', default=16, type=int)   # number of batch size for cl_model
+    args.add_argument('--batch_size_mri', default=16, type=int)     # number of batch size for mri_model
+    args.add_argument('--batch_size_cl', default=16, type=int)      # number of batch size for cl_model
     args.add_argument('--batch_size_fused', default=16, type=int)   # number of batch size for fused_model
-    args.add_argument('--n_epochs_mri', default=40, type=int)    # number of epochs for mri_model
-    args.add_argument('--n_epochs_cl', default=40, type=int)    # number of epochs for cl_model
-    args.add_argument('--n_epochs_fused', default=40, type=int)    # number of epochs for fused_model
+    args.add_argument('--n_epochs_mri', default=40, type=int)       # number of epochs for mri_model
+    args.add_argument('--n_epochs_cl', default=40, type=int)        # number of epochs for cl_model
+    args.add_argument('--n_epochs_fused', default=40, type=int)     # number of epochs for fused_model
     args.add_argument('--n_gpu', default=1, type=int)
-    args.add_argument('--seed', default=7, type=int)     # the seed to split the data into train, val, and test, and also to set the seed for reproducability of the training process
-    args.add_argument('--num_folds', default=3, type=int)  # the number of folds for cross-validation
-    args.add_argument('--fold', default=100, type=int)  # the fold number for cross-validation, starting from 0 to num_folds-1
+    args.add_argument('--seed', default=7, type=int)         # the seed to split the data into train, val, and test, and also to set the seed for reproducability of the training process
+    args.add_argument('--num_folds', default=3, type=int)    # the number of folds for cross-validation
+    args.add_argument('--fold', default=100, type=int)       # the fold number for cross-validation, starting from 0 to num_folds-1
 
     return args.parse_args()
--- run_experiment.sh
+++ run_experiment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-CODEBASE="/your/path/to/Fusion_framework"
-PROJECT_NAME='Glioma_Classification'
+CODEBASE="/your/path/to/Fusion_framework"   # the path to the codebase
+PROJECT_NAME='Glioma_Classification'        # the name of the project, it will be used for logging to Wandb
 FUSION_STRATEGY='L'     # or 'ELF' or 'ERF' or 'ISF' or 'IMF'
 DATASET_CSV_PATH='/your/path/to/dataset_metadata.csv'
 TRAIN_CSV_PATH='-'  # if you do not want to have cross-validation, you can provide the path to the csv file containing the metadata of each slice for training and change the code accordingly
@@ -14,8 +14,8 @@ NUM_CLASS=2     # binary classification
 AXIS=2          # axis along which the slices are to be taken -> 0: Sagittal, 1: Coronal, 2: Axial
 MODALITIES=("Clinical" "T1_bias" "T1c_bias" "T2_bias" "FLAIR_bias")
 SCALE_CLINICAL_MODALITY='Minmax'    # Or 'Quantile-normal' or 'Non_scaled'
-MRI_MODEL='denseNet121'     # for L, ELF, ISF, IMF, and ISF with MWB
-CL_MODEL='AutoInt'          # for L, ELF, ISF, IMF, and ISF with MWB
+MRI_MODEL='denseNet121'             # used for L, ELF, ISF, IMF, and ISF with MWB
+CL_MODEL='AutoInt'                  # used for L, ELF, ISF, IMF, and ISF with MWB
 FUSED_MODEL='majority_voting'       # or 'probability_averaging' for L, 'MLP' for ERF, 'Inter_1_concat' for ELF (the encoder layers will be freezed), 'Inter_1_concat' for ISF, 'Inter_2_concat' for IMF, and 'Inter_1_concat_attn' for ISF with MWB
 PRETRAINED=1
 LR_MRI=5e-5
@@ -38,11 +38,11 @@ LOGFILE="log_${SEED}.log"
 
 python ${CODEBASE}/train_test.py --project_name $PROJECT_NAME \
                     --fusion_method $FUSION_METHOD \
-                    --dataset_csv_path $DATASET_CSV_PATH\
+                    --dataset_csv_path $DATASET_CSV_PATH \
                     --train_csv_path $TRAIN_CSV_PATH \
                     --val_csv_path $VAL_CSV_PATH \
                     --test_csv_path $TEST_CSV_PATH \
-                    --dataset_image_path $DATASET_IMAGE_PATH\
+                    --dataset_image_path $DATASET_IMAGE_PATH \
                     --label $LABEL \
                     --num_class $NUM_CLASS \
                     --axis $AXIS \
